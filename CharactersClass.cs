bdb0f3a657c82583efa4b73c7e4d485ddd725f82
katherine Buendia <kbbuendia24@gmail.com>
2023-05-24 19:50:17 -0400

    API Project Testing

---
+++ CharactersClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using System.Drawing;
+namespace API_Project_Exercise
+{
+	public class CharactersClass
+	{
+        //public static void Name()
+        //{
+        //   var client = new HttpClient();
+
+
+        //    var mickeyUrl = "http://api.disneyapi.dev/character?page=2&pageSize=50";
+
+        //    var mickeyResponce = client.GetStringAsync(mickeyUrl).Result;
+
+        //    var mickeyObject = JObject.Parse(mickeyResponce);
+
+        //    Console.WriteLine(mickeyObject["data"][0]["name"]);
+
+
+        //}
+
+
+        public static void Name()
+        {
+            var client = new HttpClient();
+
+
+            var mickeyUrl = "https://api.disneyapi.dev/character";
+
+            var mickeyResponce = client.GetStringAsync(mickeyUrl).Result;
+
+            var mickeyObject = JObject.Parse(mickeyResponce);
+
+            // int index;
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(mickeyObject["data"][i]["name"]);
+            }
+
+           
+            //Console.WriteLine(mickeyObject["data"][1]["name"]);
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using API_Project_Exercise;
+using Newtonsoft.Json.Linq;
+
+public class Program
+{
+     static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        // character beginnigns
+
+        //var character = 0;
+        //for (int i = 0; i < 100; i++)
+        //{
+        //    CharactersClass.Name();
+        //    character++;
+        //} 
+
+        
+        
+        List<string> characterNames = new List<string>();
+        IEnumerable<string> names = characterNames.Where(name => name.Contains("name"));
+        CharactersClass.Name();
+
+
+        for (int i = 0; i < characterNames.Count(); i++)
+        {
+            CharactersClass.Name();
+            i++;
+        }
+        
+      
+        //CharacterClass names = new CharacterClass();
+    }
+}
